192f917a3497440eb7cb0d35239aab43a900d62e
Sauce <saucefishman@Stas-Gannutins-MacBook-Pro.local>
2024-08-28 10:04:34 -0500

    Add jdtls to lsp list

--- lua/user/configs/lspconfig.lua
+++ lua/user/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local servers = {'clangd', 'tsserver', 'pyright', 'rubocop', 'lua_ls', 'bashls', 'phpactor', 'cssls'}
+local servers = {'clangd', 'tsserver', 'pyright', 'rubocop', 'lua_ls', 'bashls', 'phpactor', 'cssls', 'jdtls'}
 local navbuddy = require('nvim-navbuddy')
 
 for _, lsp in ipairs(servers) do
