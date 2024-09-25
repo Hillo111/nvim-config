a66ffa8fb4455eff396bc4709e4369c2e2217515
Hillo111 <stas.gannutin@gmail.com>
2024-09-25 18:34:25 -0500

    Remove tab go to defintion

--- lua/user/configs/lspconfig.lua
+++ lua/user/configs/lspconfig.lua
@@ -110,7 +110,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gdt', "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
