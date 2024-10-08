894031a4717595e2655d36db7141cd54245c3903
Hillo111 <stas.gannutin@gmail.com>
2024-10-08 15:21:32 -0500

    ignore git

--- lua/user/configs/telescope.lua
+++ lua/user/configs/telescope.lua
@@ -4,7 +4,7 @@ local actions = require('telescope.actions')
 
 require('telescope').setup{
   defaults = {
-    file_ignore_patterns = { "dist/*", 'bin/*', 'include/*', 'node_modules/*', 'lib/*' },
+    file_ignore_patterns = { "dist/*", 'bin/*', 'include/*', 'node_modules/*', 'lib/*', '.git/*' },
   },
   pickers = {
     find_files = {
